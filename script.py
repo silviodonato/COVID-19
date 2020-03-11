5afe7627686db2c5eedadf108b4e1527c85e2ab7
Silvio Donato <silvio.donato@cern.ch>
2020-03-11 13:05:03 +0100

    update 11 March morning

--- script.py
+++ script.py
@@ -274,8 +274,8 @@ c5.Update()
 
 for place in places:
 #    savePlot(confirmes_h[place], recoveres_h[place], deaths_h[place], predictions_h[place], fits[place], fits_res[place], fits_error[place], fitexptotals[place], "plots/%s.png"%place, lastDate, c5)
-    savePlot(confirmes_h[place], recoveres_h[place], deaths_h[place], predictions_h[place], None, None, None, fitexptotals[place], "plots/%s.png"%place, lastDate, c3)
-    savePlot(newConfirmes_h[place], newRecoveres_h[place], newDeaths_h[place], None, fitdiffs[place], fitdiffs_res[place], fitdiffs_error[place], fitexps[place], "plots/%s_newCases.png"%place, lastDate, c5)
+    savePlot(confirmes_h[place], recoveres_h[place], deaths_h[place], predictions_h[place], None, None, None, None, None, None, fitexptotals[place], "plots/%s.png"%place, lastDate, c3)
+    savePlot(newConfirmes_h[place], newRecoveres_h[place], newDeaths_h[place], None, None, None, None, fitdiffs[place], fitdiffs_res[place], fitdiffs_error[place], fitexps[place], "plots/%s_newCases.png"%place, lastDate, c5)
 
 '''
 
