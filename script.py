8b5ea863effb47398ac7a211585571e802827886
Silvio Donato <silvio.donato@cern.ch>
2020-03-14 20:21:57 +0100

    Update Mar13 night

--- script.py
+++ script.py
@@ -58,7 +58,7 @@ newRecoveres = newCases(recoveres, dates)
 places = []
 for place in confirmes.keys():
     if place == "Others": continue
-    if confirmes[place][dates[lastDate]]>200:
+    if confirmes[place][dates[lastDate]]>50:
         places.append(place)
 
 #places = ["Italy","South Korea","Japan","Iran","Hubei"]
@@ -274,8 +274,12 @@ c5.Update()
 
 for place in places:
 #    savePlot(confirmes_h[place], recoveres_h[place], deaths_h[place], predictions_h[place], fits[place], fits_res[place], fits_error[place], fitexptotals[place], "plots/%s.png"%place, lastDate, c5)
-    savePlot(confirmes_h[place], recoveres_h[place], deaths_h[place], predictions_h[place], None, None, None, None, None, None, fitexptotals[place], "plots/%s.png"%place, lastDate, c3)
-    savePlot(newConfirmes_h[place], newRecoveres_h[place], newDeaths_h[place],  None, None, None, None, fitdiffs[place], fitdiffs_res[place], fitdiffs_error[place], fitexps[place], "plots/%s_newCases.png"%place, lastDate, c5)
+    shiftConf = None
+    shiftNewConf = None
+#    shiftConf = shiftHisto(confirmes_h[place], 14)
+#    shiftNewConf = shiftHisto(newConfirmes_h[place], 14)
+    savePlot(confirmes_h[place], recoveres_h[place], deaths_h[place], predictions_h[place], shiftConf, None, None, None, None, None, fitexptotals[place], "plots/%s.png"%place, lastDate, c3)
+    savePlot(newConfirmes_h[place], newRecoveres_h[place], newDeaths_h[place],  shiftNewConf, None, None, None, fitdiffs[place], fitdiffs_res[place], fitdiffs_error[place], fitexps[place], "plots/%s_newCases.png"%place, lastDate, c5)
 
 
 '''
